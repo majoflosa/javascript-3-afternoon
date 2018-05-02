6845657cafa12db516bc0e98e68a471979597a94
Mauricio Flores <contact@maurojflores.com>
2018-05-02 15:48:42 -0500

    finished higher order problems

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,7 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers  = mixedNumbers.filter( number => number % 2 === 0 );
 
 
 
@@ -44,7 +44,7 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map( price => price *= 1.07 );
 
 
 
@@ -63,7 +63,7 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce( (total, pop) => total + pop );
 
 
 
@@ -89,7 +89,7 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter( monster => monster.CP > 200 );
 
 
 
@@ -106,7 +106,12 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get the sum of all the order totals after adding in the sales tax
 */
 
-let ordersTotal //Code Here
+let ordersTotal = orders.map( order => {
+  let orderTotal = order.price + (order.price * order.tax);
+  return orderTotal;
+})
+
+// console.log( typeof ordersTotal );
 
 
 
@@ -126,6 +131,18 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.reduce( (total, purch) => {
 
+  if ( purch.owner === 'Bob' ) {
+    total += purch.price;
+  }
 
+  return total;
+
+}, 0);
+
+// let bobsTotal = purchases.reduce( (total, curr) => {
+//   return total += curr.owner === "Bob" ? curr.price : 0;
+// }, 0);
+
+console.log('bobsTotal: ', bobsTotal );
