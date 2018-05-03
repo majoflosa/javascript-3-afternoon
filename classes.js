3a1b873d73f2f218b599bd05fb651d4f3b55afa6
Mauricio Flores <contact@maurojflores.com>
2018-05-02 22:05:11 -0500

    finished classes problems

--- classes.js
+++ classes.js
@@ -30,7 +30,20 @@
 */
 
 //Code Here
+class Employee {
+  constructor( first_name, last_name, email, age ) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
 
+  makeWidget() {
+    return this.first_name + ' ' + this.last_name + ' Widget';
+  }
+}
+
+var employee = new Employee( 'John', 'Doe', 'johndoe@example.com', 35 );
 
 
 ////////// PROBLEM 2 //////////
@@ -50,7 +63,22 @@
 */
 
 //Code Here
+class Manager extends Employee {
+  constructor() {
+    super( 'Dallin', 'Anderson', 'Dallin@DallinDallin.com', 25 );
+    this.reports = [];
+  }
 
+  hire( employee ) {
+    this.reports.push( employee );
+  }
+
+  fire( index ) {
+    this.reports.splice( index, 1 );
+  }
+}
+
+var manager = new Manager;
 
 
 ////////// PROBLEM 3 //////////
@@ -76,7 +104,37 @@
 */
 
 //Code Here
+class ProgressiveManager extends Manager {
+  constructor() {
+    super( 'Dallin', 'Anderson', 'Dallin@DallinDallin.com', 25 );
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  }
 
+  hire( employee ) {
+    this.reports.push( employee );
+    
+    if ( this.reports.length === 0 ) {
+      this.title = 'Not a manager';
+    } else if ( 0 < this.reports.length && this.reports.length <= 3 ) {
+      this.title = 'Barely Manager';
+    } else if ( 3 < this.reports.length && this.reports.length <= 10 ) {
+      this.title = 'Mostly Manager';
+    } else if ( 10 < this.reports.length && this.reports.length <= 50 ) {
+      this.title = 'Manager';
+    } else if ( 50 < this.reports.length && this.reports.length <= 100 ) {
+      this.title = 'Manager Plus';
+    } else {
+      this.title = 'Bestest Manager';
+    }
+  }
+
+  fire( index ) {
+    this.reports.splice( index, 1 );
+
+    this.bonus += 100;
+  }
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -103,5 +161,42 @@
 */
 
 //Code Here
+class Machine {
+  constructor() {
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
 
+  makeWidgets( num ) {
+    this.widgets_made_count += num;
+    
+    var fiftyCounts = Math.floor(num / 50);
+    this.wear_and_tear_count += fiftyCounts;
+  }
+
+  fixMachine() {
+    this.needs_reboot = true;
+  }
+
+  reboot() {
+    // var wat = this.wear_and_tear_count
+
+    function rebootDone() {
+      this.wear_and_tear_count -= 10;
+      this.needs_reboot = false;
+
+      // console.log( 'reboot done' );
+    }
+
+    return rebootDone.bind(this);
+  }
+}
+
+var machine = new Machine();
+
+console.log( 'before reboot: ', machine.wear_and_tear_count );
+// console.log( 'reboot', machine.reboot() );
+machine.reboot();
+console.log( 'after reboot: ', machine.wear_and_tear_count );
 
